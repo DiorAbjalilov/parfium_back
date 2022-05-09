b7d5d2202ad8685f49e5b40a6f4f497bc0eb72df
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-05-09 18:05:44 +0500

    conect mongodb

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const connectDB = require("./config/db");
+
+// Env variables
+dotenv.config();
+
+// Connecting to database
+connectDB();
 
 // connect exstations
 const app = express();
-dotenv.config();
 app.use(express.json());
 app.use(cors());
 
 // api connect
-app.use("/api", require("./routers/routerController"));
+// app.use("/api", require("./routers/routerController"));
 
 // PORT
-const PORT = process.env.PORT | 5001;
+const PORT = process.env.PORT;
 app.listen(PORT, () =>
   console.log(` ========= * Success fuly * ========
  Listening on port ==> ${PORT}...`)
