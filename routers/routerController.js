652039abc6982cc20d559780ac691cc943f0b3ca
DiorAbjalilov <abjalilovdiyor@gmail.com>
2022-05-10 10:50:11 +0500

    test api

--- routers/routerController.js
+++ routers/routerController.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
+// users api connect
+router.get("/user/me/:id");
+router.post("/user/login");
+router.post("/user/sigin");
+router.put("/user/me/:id");
+router.delete("/user/me/:id");
+
+// products api connect
 router.get("/product/all");
+router.post("/product/add");
+router.put("/product/one/:id");
+router.delete("/product/one/:id");
 
 module.exports = router;
